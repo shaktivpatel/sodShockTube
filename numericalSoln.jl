5e47756cb4b09380dd3f5ef226b51448472f5b36
shaktivpatel <shaktipatel09@gmail.com>
2025-09-10 19:01:24 -0700

    Determined ICs, calculate flux vectors

--- numericalSoln.jl
+++ numericalSoln.jl
@@ -1,20 +1,68 @@
 #sod shock tube numerical solution using MacCormack (2 step Lax-Wendroff Scheme) scheme
+function shocktube()
+    #set up computational domain
+    n = 201
+    xmin = -.5
+    xmax = .5
+    dx = (xmax-xmin)/(n-1)
+    x = xmin:dx:xmax
 
-#set up computational domain
+    t = 0.0
+    tMax = .285
+    tStar = .75*tMax
 
+    #create empty variables and specificy values
 
-#set initial conditons of shock tube problem and variables
-t = .1
-x0 = 0
+    rNum = zeros(n) #density
+    uNum = zeros(n) #velocity
+    eNum = zeros(n) #specific energy
+    pNum = zeros(n) #pressure
+    cNum = zeros(n) #local speed of sound
+    U = zeros(n,3) #U vector
+    F = zeros(n,3) #flux vector
+    Ucalc = zeros(n,3) #Ubar vector
 
-rho_l = 1
-P_l = 1
-u_l = 0
+    gamma = 1.4
+    CFL = .95
 
-rho_r = .125
-P_r = .1
-u_r = 0
+    #set initial conditions
+    for i = 1:n
+        if x[i] <= 0
+            rNum[i] = 1
+            pNum[i] = 1
+            uNum[i] = 0
+        elseif x[i] > 0
+            rNum[i] = .125
+            pNum[i] = .1
+            uNum[i] = 0
+        end
 
-gamma = 1.4
-mu = sqrt((gamma - 1)/(gamma + 1))
+        eNum[i] = pNum[i]/(gamma - 1) + .5*rNum[i]*uNum[i]^2
+        cNum[i] = sqrt(gamma*pNum[i]/rNum[i])
+    end
 
+    Ucalc[:,1] = rNum
+    Ucalc[:,2] = rNum.*uNum
+    Ucalc[:,3] = eNum
+
+    while(t < tStar)
+
+        dt = CFL*dx/(maximum(cNum.+abs.(uNum)))
+
+        for i = 1:n
+
+            #calculate flux
+
+            U[i,1] = rNum[i]
+            U[i,2] = rNum[i].*uNum[i]
+            U[i,3] = eNum[i]
+
+            F[i,1] = rNum[i].*uNum[i]
+            F[i,2] = rNum[i].*uNum[i].^2 + pNum[i]
+            F[i,3] = uNum[i].*(eNum[i] + pNum[i])
+        end
+
+        t += dt
+    end
+
+end
