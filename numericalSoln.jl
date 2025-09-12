54604954f79d0d1865b14b16f1ded2a4dafb9adc
shaktivpatel <shaktipatel09@gmail.com>
2025-09-11 17:15:03 -0700

    Finished shocktube function. Need to write a script to call function and plot output

--- numericalSoln.jl
+++ numericalSoln.jl
@@ -18,8 +18,8 @@ function shocktube()
     eNum = zeros(n) #specific energy
     pNum = zeros(n) #pressure
     cNum = zeros(n) #local speed of sound
-    U = zeros(n,3) #U vector
-    F = zeros(n,3) #flux vector
+    U = zeros(n,3) #U vector [r, ru, re]
+    F = zeros(n,3) #flux vector [ru, ru^2 + p, rue + pu]
     Ucalc = zeros(n,3) #Ubar vector
 
     gamma = 1.4
@@ -61,6 +61,44 @@ function shocktube()
             F[i,2] = rNum[i].*uNum[i].^2 + pNum[i]
             F[i,3] = uNum[i].*(eNum[i] + pNum[i])
         end
+
+        for i = 2:(n-1)
+
+            #predictor
+            Ubar[i,1] = U[i,1] - (dt/dx)*(F[i+1,1] - F[i,1])
+            Ubar[i,2] = U[i,2] - (dt/dx)*(F[i+1,2] - F[i,2])
+            Ubar[i,3] = U[i,3] - (dt/dx)*(F[i+1,3] - F[i,3])
+
+            uStar[i] = Ubar[i,2]/Ubar[i,1]
+            pStar[i] = (gamma-1)*(Ubar[i,3]-(.5*Ubar[i,2]^2))/Ubar[i,1]
+            eStar[i] = (Ubar[i,3]-Ubar[i,1]*(.5*uStar[i]^2))/Ubar[i,1]
+
+            Fbar[i,1] = Ubar[i,1] .* uStar[i]
+            Fbar[i,2] = Ubar[i,1] .* uStar[i]^2 + pStar[i]
+            Fbar[i,3] = uStar[i] .* Ubar[i,3] + pStar[i]
+            Fbar[1,:] = Fbar[2,:]
+            Fbar[end,:] = Fbar[end-1,:]
+
+            #corrector
+            Ubar2[i,1] = U[i,1] - (dt/dx)*(Fbar[i,1] - Fbar[i-1,1])
+            Ubar2[i,2] = U[i,2] - (dt/dx)*(Fbar[i,2] - Fbar[i-1,2])
+            Ubar2[i,3] = U[i,3] - (dt/dx)*(Fbar[i,3] - Fbar[i-1,3])
+
+            #update
+            Ucalc[i,1] = .5 * (Ubar[i,1] + Ubar2[i,1])
+            Ucalc[i,2] = .5 * (Ubar[i,2] + Ubar2[i,2])
+            Ucalc[i,3] = .5 * (Ubar[i,3] + Ubar2[i,3])
+            Ucalc[1,:] = Ucalc[2,:]
+            Ucalc[n,:] = Ucalc[n-1,:]
+
+
+        end
+
+        rNum = Ucalc[:,1]
+        uNum = Ucalc[:,2]/rNum
+        eNum = Ucalc[:,3]
+        pNum = (gamma-1) * (eNum-.5*rNum*uNum^2)
+        cNum = sqrt(gamma*pNum/rNum)
 
         t += dt
     end
